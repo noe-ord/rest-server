3d70cd9dc71d558079ee8093781c5568ce548127
noe-ord <rodrigo.noe.ordaz@gmail.com>
2019-01-11 09:49:55 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Base de datos
 //=======================
 let urlDB;
-
-// // if(process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-    urlDB = 'mongodb://cafe-user:C0n3c74nd0@ds041671.mlab.com:41671/cafe';
-// }
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+ }//mongodb://cafe-user:C0n3c74nd0@ds041671.mlab.com:41671/cafe
 
 process.env.URLDB = urlDB;
